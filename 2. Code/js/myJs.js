19c97f6c945529b5df3527b71cbbdb9ca7b45770
hungguitar9x <hungguitar9x@gmail.com>
2013-12-24 02:58:43 +0700

    Revert "Personal Website"
    This reverts commit 9464cbc1b1864832ca83257c93de20ef5b684644.

--- 2. Code/js/myJs.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-$(window).scroll(function(e){
-  parallax();
-});
-function parallax(){
-  var scrolled = $(window).scrollTop();
-  $('.bg').css('bottom',-(scrolled*0.4)+'px');
-}
